c99a30769559e18d734a31cf4bc270547cf2f8ef
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 21:54:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,11 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   elsif
-    def take_a_number(katz_deli, other_deli)
-      other_deli.each do |name|
+    def take_a_number(katz_deli, new_customer)
+      new_customer.each do |name|
         puts "Welcome #{name}. You are number #{katz_deli.size} in line."
-        
       end
-      katz_deli << " #{katz_deli.size}. #{name}"
+      katz_deli.push(" #{katz_deli.size}. #{name}")
     end
     puts "The line is currently:" + katz_deli.join(" ")
     def now_serving(katz_deli)
