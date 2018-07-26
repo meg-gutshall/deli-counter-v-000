fe321eef1259529ca63de92d96cd2ef43f39246c
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 22:30:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   elsif
     def take_a_number(katz_deli, new_customer)
       katz_deli << new_customer
-      puts "Welcome, #{new_customer}. You are number 1 in line."
+      puts "Welcome, #{new_customer}. You are number #{katz_deli.size} in line."
     end
     def now_serving(katz_deli)
       if katz_deli.size == 0
