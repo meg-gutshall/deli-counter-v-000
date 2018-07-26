98d8ef3b2fa2ad386897894fac972246ef6c2056
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 23:17:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,11 +15,12 @@ def line(katz_deli)
         puts "Currently serving #{katz_deli.shift}."
       end
     end
+    people_in_line = []
     katz_deli.each_with_index do |name, index|
-      
+      people_in_line << "#{index + 1}. #{name}"
     end
     
-    puts "The line is currently:" + katz_deli.join(" ")
+    puts "The line is currently: " + people_in_line.join(" ")
 
   end
 end
