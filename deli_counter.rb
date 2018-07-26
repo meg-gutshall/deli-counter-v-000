6c7f3e9389a9b8011e7a8a827448a240601045ab
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 21:59:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif
     def take_a_number(katz_deli, new_customer)
-      katz_deli << " #{katz_deli.size}. #{new_customer}"
+      katz_deli = katz_deli << " #{katz_deli.size}. #{new_customer}"
     end
     puts "The line is currently:" + katz_deli.join(" ")
     def now_serving(katz_deli)
