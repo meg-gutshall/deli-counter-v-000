c6649551198388066abe92a028f35b6a3086edc1
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 21:37:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,2 @@
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli == []
-    puts "The line is currently empty."
-  else
-    def take_a_number(katz_deli, new_customer)
-      new_customer.each_with_index do |customer_name, index|
-        "Welcome, #{customer_name}. You are number #{index + 1} in line."
-        katz_deli << "#{index + 1}. #{customer_name} "
-      end
-    end
-  #   def now_serving(katz_deli, customer_name)
-    
-  #   end
-  puts "The line is currently: #{katz_deli}"
-  end
-end
