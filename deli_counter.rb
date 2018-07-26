fde03a758d75b3ad23148e71108538aceb4582b7
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 22:29:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif
     def take_a_number(katz_deli, new_customer)
+      katz_deli << new_customer
       puts "Welcome, #{new_customer}. You are number 1 in line."
     end
     def now_serving(katz_deli)
