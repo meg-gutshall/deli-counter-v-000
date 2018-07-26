f71bca3dcfac91ddc4b84b8154fe9c367c30746a
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 23:05:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
         puts "Currently serving #{katz_deli.shift}."
       end
     end
-    # puts "The line is currently:" + katz_deli.join(" ")
-    puts "The line is currently:" + (katz_deli.unshift(" #{katz_deli.size}. #{katz_deli[katz_deli.size - 1]}")).join
+    puts "The line is currently:" + katz_deli.join(" ")
+    # puts "The line is currently:" + (katz_deli.unshift(" #{katz_deli.size}. #{katz_deli[katz_deli.size - 1]}")).join
   end
 end
