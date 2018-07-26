fff5c3d89f60e10b803bf2102e4185b328859b8f
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 22:20:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
     end
     puts "The line is currently:" + katz_deli.join(" ")
     next = katz_deli.shift
-    def now_serving(next)
+    def now_serving(katz_deli)
       if katz_deli.size == 0
         puts "There is nobody waiting to be served!"
       else
